9c78c128e106a5b0161bc2f00262195f5302ad45
SanAppsMano <manosandeslimafilho@gmail.com>
2025-06-29 22:27:09 -0300

    Use undici fetch polyfill

--- functions/initFetch.js
+++ functions/initFetch.js
@@ -1,7 +1,21 @@
-import fetch from 'node-fetch';
+import {
+  fetch,
+  Headers,
+  Request,
+  Response,
+  FormData,
+  File,
+  Blob
+} from 'undici';
 
 if (!globalThis.fetch) {
   globalThis.fetch = fetch;
+  globalThis.Headers = Headers;
+  globalThis.Request = Request;
+  globalThis.Response = Response;
+  globalThis.FormData = FormData;
+  globalThis.File = File;
+  globalThis.Blob = Blob;
 }
 
 export {}; // ensures this file is a module
